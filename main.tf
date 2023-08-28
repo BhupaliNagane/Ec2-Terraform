5ddcc6750310971d6edc11ec41d47d3900b264c0
Hrishi <hrishikesh.lendave@outlook.com>
2023-08-28 15:52:19 +0200

    first

---
+++ main.tf
@@ -0,0 +1,12 @@
+providers "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_instance" "foo" {
+    ami  = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "TF-Instance"
+    }
+}
